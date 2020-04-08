42b3bb4773aefee6b18767dbb9b7be3d7b2751a6
= <=>
2020-04-08 22:08:28 +0200

    cleaned up scraper

--- lab6/scraper.py
+++ lab6/scraper.py
@@ -1,6 +1,11 @@
 import scrapy
+import os
 
 main_url = 'https://en.wikipedia.org/'
+save_to  = os.path.join(os.getcwd(), 'resources', 'test') 
+num_to_download = int(100)
+# save_to  = os.path.join(os.getcwd(), 'resources', 'docs')
+# num_to_download = int(5.2 * 1e4)
 
 ### --------------------------------|
 ### In order to execute run:        |
@@ -8,25 +13,30 @@ main_url = 'https://en.wikipedia.org/'
 ### --------------------------------|
 class WikiSpider(scrapy.Spider):
     name = "wiki_spider"
-    start_urls = [main_url + 'wiki/Mathematics']
+    start_urls = [main_url + 'wiki/Science']
 
-    num_to_download = int(1.5 * 1e4)
     set = set(start_urls[0])
 
     def parse(self, response):
         page = response.url.split('/')[-1]
 
-        filename = 'docs/{}.html'.format(page)
-        with open(filename, 'wb') as file:
+        filename      = '{}.html'.format(page)
+        full_filename = os.path.join(save_to, filename) 
+        with open(full_filename, 'wb') as file:
             file.write(response.body)
 
-        if len(self.set) >= self.num_to_download:
+        if len(self.set) >= num_to_download:
             return
 
         for link in response.css('a::attr(href)').getall():
             nodes = link.split('/')
-            if len(nodes) > 2 and nodes[1] == 'wiki' and ':' not in link and 'disambiguation' not in link:
+            if len(nodes) > 2 and nodes[1] == 'wiki' and \
+               ':' not in link and \
+               '%' not in link and \
+               'disambiguation' not in link:
+
                 next_page = main_url + link
-                if link not in self.set and len(self.set) < self.num_to_download:
+                if link not in self.set and len(self.set) < num_to_download:
                     self.set.add(link)
                     yield scrapy.Request(next_page, callback = self.parse)
+                    
